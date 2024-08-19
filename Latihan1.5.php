54fa704a40814587c81d0eb284f71ddb788596e4
amalyaputry15 <amalyaputryardhyta15@gmail.com>
2024-08-19 09:51:16 +0700

    Pertemuan 1

---
+++ Latihan1.5.php
@@ -0,0 +1,32 @@
+<?php
+class Shape
+{
+    function draw()
+    {
+    }
+}
+
+class Circle extends Shape
+{
+    function draw()
+    {
+        print "Circle has been draw.</br>";
+    }
+}
+
+class Triangle extends Shape
+{
+    function draw()
+    {
+        print "Triangle has been drawn.";
+    }
+}
+
+class Ellipse extends Shape
+{
+    function draw()
+    {
+        print "Ellipse has been drawn.";
+    }
+}
+?>
